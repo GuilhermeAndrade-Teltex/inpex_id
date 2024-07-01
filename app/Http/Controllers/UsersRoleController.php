4236bb3686b93c42a569e4e9be6d2ae525ede069
Alessia Casado <alessia.casado@teltex.com.br>
2024-07-01 14:05:13 -0300

    feat: Permissions no inserir UsersRoles

--- app/Http/Controllers/UsersRoleController.php
+++ app/Http/Controllers/UsersRoleController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUsersRoleRequest;
+use App\Models\Menus1;
+use App\Models\Menus2;
+use App\Models\UsersPermission;
 use App\Models\UsersRole;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use App\Services\AuditService;
 use App\Services\AccessLogService;
 use App\Services\ValidationService;
 use App\Services\BreadcrumbService;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use PHPUnit\Framework\Constraint\IsEmpty;
 
 class UsersRoleController extends Controller
 {
+    protected $auditService;
     protected $accessLogService;
     protected $validationService;
     protected $breadcrumbService;
 
-    public function __construct(AccessLogService $accessLogService, ValidationService $validationService, BreadcrumbService $breadcrumbService)
+    public function __construct(AccessLogService $accessLogService, AuditService $auditService, ValidationService $validationService, BreadcrumbService $breadcrumbService)
     {
+        $this->auditService = $auditService;
         $this->accessLogService = $accessLogService;
         $this->validationService = $validationService;
         $this->breadcrumbService = $breadcrumbService;
@@ -49,6 +59,15 @@ class UsersRoleController extends Controller
     {
         $this->accessLogService->logAccess("Inserir perfil");
 
+        $menus1 = Menus1::all();
+        $menus1 = $menus1->map(function ($menu) {
+            return $menu->attributesToArray();
+        })->toArray();
+        $menus2 = Menus2::all();
+        $menus2 = $menus2->map(function ($menu) {
+            return $menu->attributesToArray();
+        })->toArray();
+
         $breadcrumbsItems = [
             'Home' => 'dashboard',
             'Perfis de Usuário' => 'roles.index',
@@ -58,7 +77,7 @@ class UsersRoleController extends Controller
         $breadcrumbs = $this->breadcrumbService->generateBreadcrumbs($breadcrumbsItems);
         $pageTitle = 'Novo Perfil';
 
-        return view('pages.roles.role-create', compact('breadcrumbs', 'pageTitle'));
+        return view('pages.roles.role-create', compact('menus1', 'menus2', 'breadcrumbs', 'pageTitle'));
     }
 
     /**
@@ -66,11 +85,52 @@ class UsersRoleController extends Controller
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255'
-        ]);
+        $userRole = $request->except('menuPermissionsFirst', 'menuPermissionsSecond');
+        $permissions = $request->except('name');
 
-        UsersRole::create($validatedData);
+        $usersRole = UsersRole::create($userRole);
+        $usersRoleId = $usersRole->id;
+
+        foreach ($permissions as $permission_key => $permission_value) {
+            foreach ($permission_value as $index => $value) {
+                if ($permission_key == 'menuPermissionsFirst') {
+                    $data = [
+                        'created_by' => Auth::user()->id,
+                        'modified_by' => Auth::user()->id,
+                        'role_id' => $usersRoleId,
+                        'menu1_id' => $index,
+                        'show' => $value['view'],
+                        'edit' => $value['edit'],
+                        'create' => $value['insert'],
+                        'destroy' => $value['delete'],
+                        'export' => $value['export'],
+                        'access_log' => $value['access_log'],
+                        'audit_log' => $value['audit_log'],
+                    ];
+                    UsersPermission::create($data);
+                } else {
+                    $menu2_id = explode('_', $index);
+                    $data = [
+                        'created_by' => Auth::user()->id,
+                        'modified_by' => Auth::user()->id,
+                        'role_id' => $usersRoleId,
+                        'menu2_id' => $menu2_id[1],
+                        'show' => $value['view'],
+                        'edit' => $value['edit'],
+                        'create' => $value['insert'],
+                        'destroy' => $value['delete'],
+                        'export' => $value['export'],
+                        'access_log' => $value['access_log'],
+                        'audit_log' => $value['audit_log'],
+                    ];
+                    UsersPermission::create($data);
+                }
+            }
+        }
+
+        $data = ' inseriu um novo perfil.';
+        $this->auditService->insertLog($usersRole->id, 'usersRole', $data);
+
         return redirect()->route('roles.index')->with('success', 'Perfil de usuário criado com sucesso!');
     }
 
@@ -120,7 +180,12 @@ class UsersRoleController extends Controller
             'name' => 'required|string|max:255'
         ]);
 
+        $role_old = UsersRole::findOrFail($usersRole->id);
+        $role_old = $role_old->attributesToArray();
+
         $usersRole->update($validatedData);
+        $this->auditService->editLog($usersRole->id, 'usersRole', $role_old, $validatedData);
+
         return redirect()->route('roles.index')->with('success', 'Perfil de usuário atualizado com sucesso!');
     }
 
@@ -131,10 +196,25 @@ class UsersRoleController extends Controller
     {
         try {
             $role = UsersRole::findOrFail($id);
+            $this->auditService->destroyLog($id, 'usersRole', " deletou o perfil $role->name.");
             $role->delete();
             return response()->json(['success', 'Perfil de usuário excluído com sucesso!'], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Erro ao remover perfil: ' . $e->getMessage()], 500);
         }
+    }
+
+    /**
+     * Validate realtime request
+     */
+    public function validateUsersRoleRequestRequest(StoreUsersRoleRequest $request)
+    {
+        $validator = Validator::make($request->all(), $request->rules(), $request->messages());
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()->toArray()], 422);
+        }
+
+        return response()->json(['success' => true], 200);
     }
 }
--- resources/views/pages/roles/role-create.blade.php
+++ resources/views/pages/roles/role-create.blade.php
@@ -1,3 +1,5 @@
+@vite(['resources/js/pages/role/role-create.module.js'])
+
 @extends('layouts.app')
 
 @section('main')
@@ -6,17 +8,89 @@
 
     <div class="col-lg-12">
         <x-utils.container cardTitle="Inserir Perfil" cardSubtitle="Informações do Perfil de Usuário">
-            <form method="POST" action="{{ route('roles.create') }}">
+            <input type="hidden" id="menus" data-menu1="{{ json_encode($menus1) }}"
+                data-menu2="{{ json_encode($menus2) }}">
+            <form id="user_role_form" class="real-time-validation"
+                data-validate-url="{{ route('usersRole.validateRequest') }}" enctype="multipart/form-data">
                 @csrf
 
                 <div class="row form-group">
                     <x-form_components.input name="name" label="Nome" type_input="text" form_group_width="6" />
                 </div>
+                <div id="profile-roles-table" class="row form-group">
+                    <table class="table table-condensed table-bordered table-striped table-responsive"
+                        id="table_permissions">
+                        <thead>
+                            <tr>
+                                <th class="text-center align-items-center">Módulos</th>
+                                <th class="text-center align-items-center"><input type="checkbox" disabled></th>
+                                <th class="text-center align-items-center"><span class="fas fa-eye fa-fw"
+                                        title="Visualizar"></span><span> Visualizar</span></th>
+                                <th class="text-center align-items-center"><span class="fas fa-pencil-alt fa-fw"
+                                        title="Editar"></span><span> Editar</span></th>
+                                <th class="text-center align-items-center"><span class="fas fa-plus fa-fw"
+                                        title="Inserir"></span><span> Inserir</span></th>
+                                <th class="text-center align-items-center"><span class="fas fa-trash fa-fw"
+                                        title="Excluir"></span><span> Excluir</span></th>
+                                <th class="text-center align-items-center"><span class="fas fa-file-export fa-fw"
+                                        title="Exportar"></span><span> Exportar</span></th>
+                                <th class="text-center align-items-center"><span class="fas fa-book-open fa-fw"
+                                        title="Logs de Acesso"></span><span> Log de Acesso</span></th>
+                                <th class="text-center align-items-center"><span class="fas fa-align-justify fa-fw"
+                                        title="Histórico"></span><span> Histórico</span></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($menus1 as $menu1)
+                                <tr class="menu1" id="{{ $menu1['id'] }}">
+                                    <td class="module">
+                                        <span class="{{ $menu1['icon'] }}"></span>
+                                        <span>{{ $menu1['name'] }}</span>
+                                    </td>
+                                    <td class="text-center"><input type="checkbox" class="mark-all"></td>
+                                    <td class="text-center"><input type="checkbox" class="view"></td>
+                                    <td class="text-center"><input type="checkbox" class="edit"></td>
+                                    <td class="text-center"><input type="checkbox" class="insert">
+                                    </td>
+                                    <td class="text-center"><input type="checkbox" class="delete">
+                                    </td>
+                                    <td class="text-center"><input type="checkbox" class="export">
+                                    </td>
+                                    <td class="text-center"><input type="checkbox" class="access_log"></td>
+                                    <td class="text-center"><input type="checkbox" class="audit_log"></td>
+                                </tr>
+                                @foreach ($menus2 as $menu2)
+                                    @if ($menu2['menus1_id'] == $menu1['id'])
+                                        <tr class="menu2" id="{{ $menu1['id'] . '_' . $menu2['id']}}">
+                                            <td class="module" style="padding-left: 20px;">
+                                                <span class="{{ $menu2['icon'] }}"></span>
+                                                <span>{{ $menu2['name'] }}</span>
+                                            </td>
+                                            <td class="text-center"><input type="checkbox" class="mark-all"></td>
+                                            <td class="text-center"><input type="checkbox" class="view">
+                                            </td>
+                                            <td class="text-center"><input type="checkbox" class="edit">
+                                            </td>
+                                            <td class="text-center"><input type="checkbox" class="insert"></td>
+                                            <td class="text-center"><input type="checkbox" class="delete"></td>
+                                            <td class="text-center"><input type="checkbox" class="export"></td>
+                                            <td class="text-center"><input type="checkbox" class="access_log">
+                                            </td>
+                                            <td class="text-center"><input type="checkbox" class="audit_log">
+                                            </td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
 
                 <div class="row justify-content-end">
                     <div class="col-lg-6">
                         <div class="text-end">
-                            <x-utils.btn tagHtml="button" type="submit" color="primary" text="Salvar" />
+                            <x-utils.btn tagHtml="button" type="submit" color="primary" text="Salvar"
+                                id="usersRoleBtn" />
                             <x-utils.btn tagHtml="a" ref="{{ route('roles.index') }}" color="default" text="Cancelar" />
                         </div>
                     </div>
